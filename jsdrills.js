3631f75615330808103053b1b0767bf95aa9c929
Jamie Parsons <jhelenparsons@gmail.com>
2017-05-25 20:52:00 -0600

    commit1

--- jsdrills.js
+++ jsdrills.js
@@ -18,10 +18,20 @@ array.forEach(function(num) {
 console.log(sum)
 
 
-
 // Fahrenheit to Celsius
 
  function fToC (a) {
      return (a - 32) * (5 / 9)
  };
  console.log(fToC(80));
+
+
+// .  Average of an array
+
+var array = [1,2,4,3,-15]
+var sum = 0;
+array.forEach(function(num){
+  sum += num;
+  avg = sum / array.length;
+})
+console.log(avg)
