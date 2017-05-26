66d9b1085334e2c84a83b8582c29434688997ced
Jamie Parsons <jhelenparsons@gmail.com>
2017-05-25 20:43:16 -0600

    commit

--- jsdrills.js
+++ jsdrills.js
@@ -1,11 +1,22 @@
 // JS Drills
 
+
 // Add
 
 function add (x, y) {
   return x + y;
 }
 console.log(add(4,6));
+
+// Sum Array
+
+var array = [1,3,5,7,1,-9];
+var sum = 0;
+array.forEach(function(num) {
+  sum += num;
+});
+console.log(sum)
+
 
 
 // Fahrenheit to Celsius
@@ -14,5 +25,3 @@ console.log(add(4,6));
      return (a - 32) * (5 / 9)
  };
  console.log(fToC(80));
-
- //
