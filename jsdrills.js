6cc940a7ea331bb58dcdcbb64f73f029939f8557
Jamie Parsons <jhelenparsons@gmail.com>
2017-05-25 20:30:37 -0600

    first commit

---
+++ jsdrills.js
@@ -0,0 +1,18 @@
+// JS Drills
+
+// Add
+
+function add (x, y) {
+  return x + y;
+}
+console.log(add(4,6));
+
+
+// Fahrenheit to Celsius
+
+ function fToC (a) {
+     return (a - 32) * (5 / 9)
+ };
+ console.log(fToC(80));
+
+ //
